f1570058ca86b45dc76e29ed8669154ba6357d10
xc106d3 <xc106d3@xc106.local>
2018-03-20 05:04:02 -0400

    Minor changes

--- swiftC/ViewController.swift
+++ swiftC/ViewController.swift
@@ -8,13 +8,51 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        Data.shared.appendCity()
     }
 
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Data.shared.cities.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 45.0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CityCell", for: indexPath)
+        let city = Data.shared.cities[indexPath.row]
+        cell.textLabel?.text = "\(city.name)" + " with \(city.id)"
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let cityVC: CityViewController? = segue.destination as? CityViewController
+        
+        let indexPath = tableView.indexPathForSelectedRow
+        let city = Data.shared.cities[indexPath!.row]
+        
+        cityVC?.city = city
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
