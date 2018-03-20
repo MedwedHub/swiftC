501454c06c5b5654a0463ddc42b4f3951c7e36d9
xc106d3 <xc106d3@xc106.local>
2018-03-20 04:22:26 -0400

    Initial Commit

---
+++ swiftC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftC
+//
+//  Created by xc106d3 on 2018-03-20.
+//  Copyright © 2018 xc106d3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
