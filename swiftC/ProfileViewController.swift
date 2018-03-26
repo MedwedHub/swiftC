26caf2f9c9dc8d351b968920ffd9cb216606d321
xc106d3 <xc106d3@xc106.local>
2018-03-26 12:34:53 -0400

    Some changes

--- swiftC/ProfileViewController.swift
+++ swiftC/ProfileViewController.swift
@@ -24,12 +24,35 @@ class ProfileViewController: UIViewController {
         birthField.inputView = datePicker
         userManager = UserManager()
         user = userManager.getCurrentUser()
+        nameField.text = user.name
+        let dateToString: String
+        if let date = user.birthDay {
+            let dateFormatter = DateFormatter()
+            dateToString = dateFormatter.string(from: date)
+            birthField.text = dateToString
+        } else {
+            birthField.text = ""
+        }
     }
     func configureDate() {
         datePicker = UIDatePicker(frame: CGRect.zero)
         datePicker.datePickerMode = .date
         datePicker.addTarget(self, action: #selector(onDatePickerChanged), for: UIControlEvents.valueChanged)
-    }
+        let accView = UIView()
+        accView.frame = CGRect(x: 0, y: 0, width: 0, height: 30)
+        accView.layer.backgroundColor = UIColor.lightGray.cgColor
+        let accButton = UIButton()
+        accButton.setTitle("Done", for: .normal)
+        accButton.frame = CGRect(x: self.view.frame.width - 70, y: 5, width: 50, height: 20)
+        accButton.addTarget(self, action: #selector(birthFieldDidEndEditing), for: .touchUpInside)
+        accView.addSubview(accButton)
+        birthField.inputAccessoryView = accView
+        
+        if let name = UserDefaults.standard.string(forKey: "nameKey"){
+            print(name)
+        } else {
+            print("There is no name in database")
+        }    }
     @objc func onDatePickerChanged(_ datePicker: UIDatePicker) {
         let date = datePicker.date
         let dateFormatter = DateFormatter()
@@ -41,10 +64,18 @@ class ProfileViewController: UIViewController {
     func textFieldDidEndEditing(_ textField: UITextField) {
         if let text = textField.text, text.count > 0 {
             user.name = text
-            userManager.changeUser(user)        
+            userManager.changeUser(user)
+            print("Username changed")
+            if let name = UserDefaults.standard.string(forKey: "nameKey"){
+            print(name)
+            } else {
+                print("There is no name in database")
+            }
         }
     }
-    
+    @objc func birthFieldDidEndEditing(sender: Any) {
+        self.view.endEditing(true)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
