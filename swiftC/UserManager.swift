1755abb87666672a8915360574c8b58e3a59d747
MedwedHub <37138105+MedwedHub@users.noreply.github.com>
2018-03-29 08:30:54 +0700

    Update UserManager.swift

--- swiftC/UserManager.swift
+++ swiftC/UserManager.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class UserManager {
+/*class UserManager {
     private let user = User()
     
     func getCurrentUser() -> User {
@@ -22,4 +22,22 @@ class UserManager {
         UserDefaults.standard.set(user.name, forKey: "nameKey")
         UserDefaults.standard.set(user.birthDay, forKey: "birthDayKey")
     }
+}*/
+
+class UserManager {
+
+  var user: User {
+    get {
+      let name = UserDefaults.standard.string(forKey: "nameKey")
+      let birthDay = UserDefaults.standard.object(forKey: "birthDayKey") as? Date
+      let user = User(name: name, birthDay: birthDay, avatar: nil)
+
+      return user
+    }
+
+    set {
+      UserDefaults.standard.set(newValue.name, forKey: "nameKey")
+      UserDefaults.standard.set(newValue.birthDay, forKey: "birthDayKey")
+    }
+  }
 }
